9eb1b3b7103d25c5cdce68bfb4e04dd7b82a1b28
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2022-09-18 18:57:54 +0530

    errors errors errors 🥲

--- frontend/pages/marketplace.jsx
+++ frontend/pages/marketplace.jsx
@@ -10,6 +10,13 @@ import { isAddress } from "ethers/lib/utils";
 import { ethers } from "ethers";
 import { useContract, useContractRead } from "@thirdweb-dev/react";
 
+// import { useContract } from "@thirdweb-dev/react";
+// import React, { useState, useEffect } from "react";
+/// same collection address for all the NFTs created by the artists
+// import { NFT_Contract_adddress, Token_abi } from "../constants";
+// import { useProvider, useSigner, useContract, useAccount } from "wagmi";
+// import { useContract, useContractRead } from "@thirdweb-dev/react";
+// import { ethers } from "ethers";
 
 
 export default function marketplace() {
@@ -19,6 +26,204 @@ export default function marketplace() {
     setToggleState(index);
     console.log(index);
   }
+
+  // fetch Tokens
+
+  const [tokens, setTokens] = useState([]);
+
+  const { NFT_contract } = useContract(
+    "0xF99FcE9c34d8ed38108425Ce39B6D4d4Cd3cb470"
+  );
+  const { Fraction_contract } = useContract(
+    "0x787FD6F86c692B8FbB0452B399fd5302201BFf79"
+  );
+
+  const { address, isConnected } = useAccount();
+  const provider = useProvider();
+  const { data: signer } = useSigner();
+
+  useEffect(async () => {}, []);
+
+  async function fetchNFTs() {
+    try {
+      const { data, isLoading } = useContractRead(
+        Fraction_contract,
+        "getTokens"
+      );
+      // array of all the addresses
+      const promises = [];
+      data.map((token) => {
+        const response = checkToken(token);
+        if (response == true) {
+          const data = fetchTokenData(token);
+          promises.push(data);
+        }
+      });
+      const _tokens = await Promise.all(promises);
+
+      /// filtered and all the needed data is fetched and stored as objects in the array of NFT
+      console.log(_nfts);
+      setTokens(_tokens);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  const fetchTokenData = async ({ _tokenAddress }) => {
+    try {
+      const Contract = useContract({
+        addressOrName: _tokenAddress,
+        contractInterface: Token_abi,
+        signerOrProvider: signer || provider,
+      });
+
+      // fetch the tokenId
+      const data = await Contract.tokenId();
+      const tokenId = parseInt(data.hex._value);
+
+      const _price = await Token_Contract.tokenPrice();
+      const price_ = parseInt(_price.hex._value);
+
+      const _sold = await Token_Contract.sold();
+      const sold_ = parseInt(_sold.hex._value);
+
+      const _total = await Token_Contract.total();
+      const total_ = parseInt(_total.hex._value);
+
+      // filter the address first
+      const { response } = useContractRead(NFT_contract, "tokenURI", tokenId);
+      console.log(response);
+      /// filter the NFT URI from the link and then
+
+      const metadata = await fetch(tokenURI);
+      const metadataJSON = await metadata.json();
+      console.log(metadataJSON);
+
+      const Token = {
+        name: metadataJSON.name,
+        description: metadataJSON.description,
+        image: metadataJSON.image,
+        price: ethers.utils.formatEther(price_),
+        sold: sold_,
+        total: total_,
+        tokenAddress: _tokenAddress,
+        tokenId: tokenId,
+        collectionAddress: NFT_Contract_adddress,
+      };
+
+      return Token;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const checkToken = async ({ _tokenAddress }) => {
+    try {
+      const Token_Contract = useContract({
+        addressOrName: _tokenAddress,
+        contractInterface: Token_abi,
+        signerOrProvider: signer || provider,
+      });
+
+      const data = await Token_Contract.tokenSaleStarted();
+
+      /// return whether true or not
+      return data;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+
+// fetch NFTs
+
+const [nfts, setNfts] = useState([]);
+
+
+
+useEffect(async () => {}, []);
+
+  async function fetchNFTs() {
+    try {
+      const { data, isLoading } = useContractRead(
+        Fraction_contract,
+        "getTokens"
+      );
+      // array of all the addresses
+      const promises = [];
+      data.map((nft) => {
+        const response = checkToken(nft);
+        if (response == true) {
+          const data = fetchTokenData(nft);
+          promises.push(data);
+        }
+      });
+      const _nfts = await Promise.all(promises);
+
+      /// filtered and all the needed data is fetched and stored as objects in the array of NFT
+      console.log(_nfts);
+      setNfts(_nfts);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async function fetchTokenData({ _tokenAddress }) {
+    try {
+      const Contract = useContract({
+        addressOrName: _tokenAddress,
+        contractInterface: Token_abi,
+        signerOrProvider: signer || provider,
+      });
+
+      // fetch the tokenId
+      const data = await Contract.tokenId();
+      const tokenId = parseInt(data.hex._value);
+
+      /// price is stored in form of wei
+      const _price = await Token_Contract.salePrice();
+      const price_ = parseInt(_price.hex._value);
+      // filter the address first
+      const { response } = useContractRead(NFT_contract, "tokenURI", tokenId);
+      console.log(response);
+      /// filter the NFT URI from the link and then
+      const metadata = await fetch(tokenURI);
+      const metadataJSON = await metadata.json();
+      console.log(metadataJSON);
+
+      const NFT = {
+        name: metadataJSON.name,
+        description: metadataJSON.description,
+        image: metadataJSON.image,
+        price: ethers.utils.formatEther(price_),
+        tokenAddress: _tokenAddress,
+        tokenId: tokenId,
+        collectionAddress: NFT_Contract_adddress,
+      };
+
+      return NFT;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async function checkToken({ _tokenAddress }) {
+    try {
+      const Token_Contract = useContract({
+        addressOrName: _tokenAddress,
+        contractInterface: Token_abi,
+        signerOrProvider: signer || provider,
+      });
+
+      const data = await Token_Contract.saleStarted();
+
+      /// return whether true or not
+      return data;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <div className={styles.main}>
       <h1 className={styles.heading}>Marketplace</h1>
--- frontend/pages/nft/[id].jsx
+++ frontend/pages/nft/[id].jsx
@@ -1,9 +1,93 @@
-import React from "react";
 import styles from "../../styles/Home.module.css";
 import image from "../../src/assets/3.png";
 import Image from "next/image";
 
+import { useContract } from "@thirdweb-dev/react";
+import React, { useState, useEffect } from "react";
+/// same collection address for all the NFTs created by the artists
+import { NFT_Contract_adddress, Token_abi } from "../constants";
+import { isAddress } from "ethers/lib/utils";
+import { useProvider, useSigner, useContract, useAccount } from "wagmi";
+import { ethers } from "ethers";
+import { useRouter } from "next/router";
+import {
+  useContract,
+  useContractRead,
+  useContractWrite,
+} from "@thirdweb-dev/react";
+
 export default function NFT(props) {
+
+  // buy nft
+
+  const [tokenAddress, settokenAddress] = useState("");
+  const [nfPrice, setnfPrice] = useState(0);
+  const [price, setPrice] = useState("");
+  const [data, setData] = useState({});
+
+  const router = useRouter();
+  const nftAddress = router.query.nftContract;
+  const tokenId = router.query.tokenId;
+
+  const { NFT_contract } = useContract(
+    "0xF99FcE9c34d8ed38108425Ce39B6D4d4Cd3cb470"
+  );
+  const { Fraction_contract } = useContract(
+    "0x787FD6F86c692B8FbB0452B399fd5302201BFf79"
+  );
+
+  const { address, isConnected } = useAccount();
+  const provider = useProvider();
+  const { data: signer } = useSigner();
+
+  const fetchTokenData = async ({ _tokenAddress }) => {
+    try {
+      const { address } = useContractRead(
+        Fraction_contract,
+        "getAddress",
+        nftAddress,
+        tokenId
+      );
+
+      // filter the address first
+      settokenAddress(address);
+
+      const { response } = useContractRead(NFT_contract, "tokenURI", tokenId);
+      console.log(response);
+      /// filter the NFT URI from the link and then
+
+      const metadata = await fetch(tokenURI);
+      const metadataJSON = await metadata.json();
+      console.log(metadataJSON);
+      setData(metadataJSON);
+
+      const Token_Contract = useContract({
+        addressOrName: tokenAddress,
+        contractInterface: Token_abi,
+        signerOrProvider: signer || provider,
+      });
+
+      const _price = await Token_Contract.salePrice();
+      const price = parseInt(_price.hex._value);
+      setnfPrice(price);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const buyNFT = async () => {
+    try {
+      console.log("Buying NFT .. ");
+      const tx = await Token_Contract.purchaseNFT({
+        value: ethers.utils.parseEther(price),
+      });
+      await tx.wait();
+      console.log("NFT purchase complete");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className={styles.nft_page}>
       <h1 className={styles.heading}>NFT title here</h1>
