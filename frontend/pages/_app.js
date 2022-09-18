d3dc3fcf3a09f111e7ed7688581a2e163f6153ed
kushagrasarathe <76868364+kushagrasarathe@users.noreply.github.com>
2022-09-18 23:27:46 +0530

    favicon and description

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -8,6 +8,7 @@ import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 import { ThirdwebProvider, ChainId } from "@thirdweb-dev/react";
+import Head from "next/head";
 
 const desiredChainId = ChainId.Mumbai;
 
@@ -30,6 +31,17 @@ const wagmiClient = createClient({
 function MyApp({ Component, pageProps }) {
   return (
     <ThirdwebProvider desiredChainId={desiredChainId}>
+      <Head>
+        <title>Artist3</title>
+        <meta
+          name="description"
+          content="Mint NFT as a proof for your digital artwork in simple steps and
+              sell it in form of fractions directly on the platform to earn from
+              it. Token Holders earn a profit by holding the tokens for a peroid
+              of time"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <WagmiConfig client={wagmiClient}>
         <RainbowKitProvider chains={chains}>
           <Layout>
